06e4eccdd19989fe75874616cd3cc26f2d876f0e
Mahadi001 <sadat.himel@gmail.com>
2021-08-22 17:04:35 +0600

    some update

--- our-metabox.php
+++ our-metabox.php
@@ -26,7 +26,7 @@ class OurMetabox {
         if($location==''){
             return $post_id;
         }
-        add_post_meta( $post_id, 'omb_location', $location);
+        update_post_meta( $post_id, 'omb_location', $location);
     }
 
    public function omb_add_metabox(){
@@ -40,16 +40,15 @@ class OurMetabox {
             );
     }
 
-    public function omb_display_post_location()
+    public function omb_display_post_location($post)
     {
+        $location = get_post_meta( $post->ID, 'omb_location', true);
         $label = __("Location","our-metabox");
-        $email =__("Email", "our-metabox");
+        wp_nonce_field( 'omb_location', 'omb_location_field');
         $metabox_html = <<<EOD
             <P>
                 <label for="omb_location">{$label}:</label>
-                <input type="text" name="omb_location" id="omb_location" /></br>
-                <label for="omb_location">{$email}</label>
-                <input type="email" name="omb_location" id="omb_location" />
+                <input type="text" name="omb_location" id="omb_location" value="{$location}" /></br>
             </p>
         EOD;
 
