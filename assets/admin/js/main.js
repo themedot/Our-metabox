a12e54428750988ff4fff7c62c17a312fe578095
Mahadi001 <sadat.himel@gmail.com>
2021-08-25 17:41:18 +0600

    update

--- assets/admin/js/main.js
+++ assets/admin/js/main.js
@@ -20,6 +20,15 @@ var frame;
             },
             multiple:false
           });
+
+          frame.on('select',function(){
+            attachment = frame.state().get('selection').first().toJSON();
+            $("#omb_image_id").val(attachment.id);
+            $("#omb_image_url").val(attachment.sizes.thumbnail.url);
+            console.log(attachment);
+            $("#image_container").html(`<img src="${attachment.sizes.thumbnail.url}" id='${attachment.id}' />`);
+          });
+
           frame.open();
           return false;
         });
